db6eea83b8b180a28fab59555baef5b476aa0416
Pritam <sugandhepritam10@gmail.com>
2022-06-19 01:39:10 +0530

    done

--- arm.java
+++ arm.java
@@ -1,13 +1,4 @@
-mport jva.utildlkkhldkfh*;
-	public static void main(String[]ah)
-	{
-		
-		Scanner kjhkjhajbc,mob=new Scanner(System.in);
-		
-		System.out.println("Enter the number that you want to check ");
-		int n=ob.nextInt();
-		
-	 	int a=n;
+mn;
 		int sum=0,rem;
 		while(a>0)
 		{
